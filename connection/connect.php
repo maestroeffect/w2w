cb05175df4252899e1a16cdb837cdd7a17452577
maestroeffect <pomokhaiye8@gmail.com>
2024-03-07 17:04:12 +0100

    first commit

---
+++ connection/connect.php
@@ -0,0 +1,10 @@
+<?php 
+
+$hostname = "localhost";
+$database = "w2w";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($hostname, $username, $password, $database);
+
+?>
---
+++ js/main.js
@@ -0,0 +1,108 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+    // Animated typing text
+
+    $(".animated-text").typed({
+        strings: [
+            "Welcome To Chaplaincy Waste to Wealth",
+            "Where Waste Meets Innovation - Your Green Adventure Begins!",
+            "Transforming Waste into Treasure - Join Us Today!"
+        ],
+        typeSpeed: 40,
+        loop: true,
+    });
+
+    const text = document.querySelector(".sec-text");
+
+        const textLoad = () => {
+            setTimeout(() => {
+                text.textContent = "Chaplaincy Waste To Wealth";
+            }, 0);
+            setTimeout(() => {
+                text.textContent = "Chaplaincy Waste 2 Wealth";
+            }, 4000);
+            setTimeout(() => {
+                text.textContent = "CW2W";
+            }, 8000); //1s = 1000 milliseconds
+        }
+
+        textLoad();
+        setInterval(textLoad, 12000);
+
+    
+})(jQuery);
+
